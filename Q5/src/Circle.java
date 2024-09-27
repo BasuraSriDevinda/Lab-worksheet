b2421c04163d58e34aa53c3ae71f6d78b8bcebf3
BasuraSriDevinda <basura19780613@gmail.com>
2024-09-27 15:19:40 +0530

    Add files via upload

---
+++ Q5/src/Circle.java
@@ -0,0 +1,24 @@
+
+public class Circle {
+
+    int area;
+    int radii;
+    int circumference;
+    final double pi=Math.PI;
+    public void computeArea(){
+        area=(int )(pi*radii*radii);
+
+    }
+    public void computeCircumference(){
+        circumference=(int )(2*pi*radii);
+
+    }
+
+
+
+    public void setRadii(int radii) {
+        this.radii = radii;
+    }
+
+
+}
